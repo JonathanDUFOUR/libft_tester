fe49356235687f33421ff0a740d562e41da49e5f
jodufour <jodufour@student.42.fr>
2021-03-07 23:20:21 +0100

    New tests

--- srcs/test_ft_cut_spaces.c
+++ srcs/test_ft_cut_spaces.c
@@ -6,17 +6,65 @@
 /*   By: jodufour <jodufour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/07 19:15:02 by jodufour          #+#    #+#             */
-/*   Updated: 2021/03/07 19:27:51 by jodufour         ###   ########.fr       */
+/*   Updated: 2021/03/07 23:11:02 by jodufour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 #include "libft.h"
 
 void	test_ft_cut_spaces(void)
 {
+	char	*s;
+	char	*res;
+
 	ft_putendl("************************");
 	ft_putendl("*                      *");
 	ft_putendl("*     FT_CUT_SPACES    *");
 	ft_putendl("*                      *");
 	ft_putendl("************************");
+
+	ft_putendl("Test 00");
+	s = ft_strdup("");
+	res = ft_cut_spaces(s);
+	ft_putstr("ft_cut_spaces(\"");
+	ft_putstr(s);
+	ft_putstr("\") == \"");
+	ft_putstr(res);
+	ft_putendl("\"\n");
+	free(s);
+	free(res);
+
+	ft_putendl("Test 01");
+	s = ft_strdup("   ");
+	res = ft_cut_spaces(s);
+	ft_putstr("ft_cut_spaces(\"");
+	ft_putstr(s);
+	ft_putstr("\") == \"");
+	ft_putstr(res);
+	ft_putendl("\"\n");
+	free(s);
+	free(res);
+
+	ft_putendl("Test 02");
+	s = ft_strdup("HelloWorld");
+	res = ft_cut_spaces(s);
+	ft_putstr("ft_cut_spaces(\"");
+	ft_putstr(s);
+	ft_putstr("\") == \"");
+	ft_putstr(res);
+	ft_putendl("\"\n");
+	free(s);
+	free(res);
+
+	ft_putendl("Test 03");
+	s = ft_strdup("  Coucou		les	amis  \n!");
+	res = ft_cut_spaces(s);
+	ft_putstr("ft_cut_spaces(\"");
+	ft_putstr(s);
+	ft_putstr("\") == \"");
+	ft_putstr(res);
+	ft_putendl("\"\n");
+	free(s);
+	free(res);
 }
--- srcs/test_ft_file_size.c
+++ srcs/test_ft_file_size.c
@@ -6,17 +6,56 @@
 /*   By: jodufour <jodufour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/07 19:16:04 by jodufour          #+#    #+#             */
-/*   Updated: 2021/03/07 19:17:09 by jodufour         ###   ########.fr       */
+/*   Updated: 2021/03/07 23:19:25 by jodufour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 #include "libft.h"
 
 void	test_ft_file_size(void)
 {
+	char	*f;
+
 	ft_putendl("************************");
 	ft_putendl("*                      *");
 	ft_putendl("*     FT_FILE_SIZE     *");
 	ft_putendl("*                      *");
 	ft_putendl("************************");
+
+	ft_putendl("Test 00");
+	f = ft_strdup("./ressources/Lorem_Ipsum");
+	ft_putstr("ft_file_size(\"");
+	ft_putstr(f);
+	ft_putstr("\") == ");
+	ft_putlnbr(ft_file_size(f));
+	free(f);
+	ft_putstr("\n\n");
+
+	ft_putendl("Test 01");
+	f = ft_strdup("./ressources/empty");
+	ft_putstr("ft_file_size(\"");
+	ft_putstr(f);
+	ft_putstr("\") == ");
+	ft_putlnbr(ft_file_size(f));
+	free(f);
+	ft_putstr("\n\n");
+
+	ft_putendl("Test 02");
+	f = ft_strdup("./ressources/only_42");
+	ft_putstr("ft_file_size(\"");
+	ft_putstr(f);
+	ft_putstr("\") == ");
+	ft_putlnbr(ft_file_size(f));
+	free(f);
+	ft_putstr("\n\n");
+
+	ft_putendl("Test 03");
+	f = ft_strdup("./Makefile");
+	ft_putstr("ft_file_size(\"");
+	ft_putstr(f);
+	ft_putstr("\") == ");
+	ft_putlnbr(ft_file_size(f));
+	free(f);
+	ft_putstr("\n\n");
 }
